d80c94f04f94d2d8974710c3047a6d764584028e
madsq98-desktop <mads.q@outlook.com>
2022-09-20 15:59:28 +0200

    stuff

--- src/app/admin/posts/posts-read/posts-read.component.ts
+++ src/app/admin/posts/posts-read/posts-read.component.ts
@@ -5,6 +5,10 @@ import {PostsService} from "../../../shared/postsService/posts.service";
 import {CategoriesService} from "../../../shared/categoriesService/categories.service";
 import {CategoryDto} from "../../../shared/categoriesService/categories.dto";
 import {PostsHeaderSort, compare, SortEvent} from "./postsHeaderSort.directive";
+import {UsersService} from "../../../shared/usersService/users.service";
+import {UserDto} from "../../../shared/usersService/user.dto";
+import {UntypedFormControl, UntypedFormGroup} from "@angular/forms";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-posts-read',
@@ -12,30 +16,59 @@ import {PostsHeaderSort, compare, SortEvent} from "./postsHeaderSort.directive";
   styleUrls: ['./posts-read.component.css']
 })
 export class PostsReadComponent implements OnInit {
+  filterForm = new UntypedFormGroup({
+    category: new UntypedFormControl('0'),
+    author: new UntypedFormControl('0'),
+    paid: new UntypedFormControl('0')
+  });
+
   filter!: string;
   data: Array<PostSortingModel> = new Array<PostSortingModel>();
   postsArray: Array<PostSortingModel> = new Array<PostSortingModel>();
 
   posts$: Observable<PostDto[]> | undefined;
+
+  categories$: Observable<CategoryDto[]> | undefined;
+  users$: Observable<UserDto[]> | undefined;
   error: any;
 
   @ViewChildren(PostsHeaderSort)
   headers!: QueryList<PostsHeaderSort>;
 
-  constructor(private _postService: PostsService, private _categoryService: CategoriesService) { }
+  constructor(private _postService: PostsService, private _categoryService: CategoriesService, private _usersService: UsersService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.categories$ = this._categoryService.getAll();
+    this.users$ = this._usersService.getAll();
+
     this._postService.getAll()
       .subscribe(data => {
+        // @ts-ignore
+        this.categories$.subscribe(c => {
+          this.route.queryParams
+            .subscribe(params => {
+                let categoryId : number = params['category'];
+                if(categoryId > 0) {
+                  this.filterForm.controls['category'].setValue(categoryId);
+                  this.doFilter();
+                  this.filterForm.controls['category'].setValue(categoryId + ": " + categoryId);
+                }
+              }
+            );
+        });
+
         data.forEach(obj => {
           let newPost = {
             id: obj.id,
             categoryName: obj.category.title,
+            categoryId: obj.category.id,
             title: obj.title,
             prettyDescriptor: obj.prettyDescriptor,
             authorName: obj.author.firstname + " " + obj.author.lastname,
+            authorId: obj.author.id,
             date: obj.date,
-            paid: obj.paid
+            paid: obj.paid,
+            featuredImageUrl: obj.featuredImageUrl
           } as PostSortingModel
           this.postsArray.push(newPost)
           this.data = this.postsArray;
@@ -68,6 +101,32 @@ export class PostsReadComponent implements OnInit {
       });
     }
   }
+
+  doFilter() {
+    let categoryValue : number = this.filterForm.get('category')?.value;
+    let authorValue : number = this.filterForm.get('author')?.value;
+    let paidValue : number = this.filterForm.get('paid')?.value;
+
+    let data = this.data;
+
+    if(categoryValue > 0) {
+      data = data.filter(obj => obj.categoryId == categoryValue);
+    }
+
+    if(authorValue > 0) {
+      data = data.filter(obj => obj.authorId == authorValue);
+    }
+
+    if(paidValue > 0) {
+      data = data.filter(obj => obj.paid == (paidValue == 1));
+    }
+
+    this.postsArray = data;
+  }
+
+  doReset() {
+    this.postsArray = this.data;
+  }
 }
 export interface PostSortingModel {
   id: number;
@@ -77,4 +136,7 @@ export interface PostSortingModel {
   authorName: string;
   date: Date;
   paid: boolean;
+  categoryId: number;
+  authorId: number;
+  featuredImageUrl: string;
 }
