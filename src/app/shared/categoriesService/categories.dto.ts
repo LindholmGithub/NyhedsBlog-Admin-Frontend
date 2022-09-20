38b0efaa4f83429e0fbb4c20a9fcbaabe6cd5fd9
madsq98-desktop <mads.q@outlook.com>
2022-09-20 14:36:29 +0200

    stuff

--- src/app/shared/categoriesService/categories.dto.ts
+++ src/app/shared/categoriesService/categories.dto.ts
@@ -3,6 +3,7 @@ import {PostDto} from "../postsService/posts.dto";
 export interface CategoryDto {
   id: number;
   title: string;
+  featured: boolean;
   description: string;
   prettyDescriptor: string;
   posts: PostDto[];
